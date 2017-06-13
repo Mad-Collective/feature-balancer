11adb187a5a1cbf0762881a312f1efb652fc431f
Hilari Moragrega <hilari.moragrega@teamcmp.com>
2017-06-13 12:32:30 +0200

    Added builder and readme

---
+++ spec/BalancerBuilderSpec.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace spec\Cmp\FeatureBalancer;
+
+use Cmp\FeatureBalancer\Exception\OutOfBoundsException;
+use PhpSpec\ObjectBehavior;
+
+class BalancerSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->add("foo", [
+            "abc" => 80,
+            "def" => 20,
+        ]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Cmp\FeatureBalancer\Balancer');
+    }
+
+    function it_can_distribute_hits_across_features_based_on_numeric_seeds()
+    {
+        $this->get("foo", 0)->shouldReturn("abc");
+        $this->get("foo", 1)->shouldReturn("abc");
+        $this->get("foo", 79)->shouldReturn("abc");
+
+        $this->get("foo", 80)->shouldReturn("def");
+        $this->get("foo", 99)->shouldReturn("def");
+
+        $this->get("foo", 100)->shouldReturn("abc");
+    }
+
+    function it_can_distribute_hits_across_features_based_on_string_seeds()
+    {
+        $this->get("foo", "bar")->shouldReturn("abc");
+        $this->get("foo", "aaa")->shouldReturn("def");
+    }
+
+    function it_can_distribute_hits_across_features_without_seed()
+    {
+        $this->get("foo")->shouldMatch('/abc|def/');
+    }
+
+    function it_fails_if_the_requested_feature_has_not_been_added()
+    {
+        $this->shouldThrow(OutOfBoundsException::class)->duringGet("bar");
+    }
+}
---
+++ src/BalancerBuilder.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Cmp\FeatureBalancer;
+
+use Cmp\FeatureBalancer\Config\Identifier;
+use Cmp\FeatureBalancer\Decorator\LoggerDecorator;
+use Cmp\FeatureBalancer\Decorator\MonitoringDecorator;
+use Cmp\Monitoring\Monitor;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+class BalancerBuilder
+{
+    /**
+     * @var Monitor|null
+     */
+    private $monitor;
+
+    /**
+     * @var string|null
+     */
+    private $metric;
+
+    /**
+     * @var LoggerInterface|null
+     */
+    private $logger;
+
+    /**
+     * @var string|null
+     */
+    private $logLevel;
+
+    /**
+     * @param array $config
+     *
+     * @return Balancer|LoggerDecorator|MonitoringDecorator
+     */
+    public function create(array $config = [])
+    {
+        $balancer = new Balancer();
+        $this->addConfig($balancer, $config);
+
+        if ($this->monitor instanceof Monitor) {
+            $balancer = new MonitoringDecorator($balancer, $this->monitor, $this->metric);
+        }
+
+        if ($this->logger instanceof LoggerInterface) {
+            $balancer = new LoggerDecorator($balancer, $this->logger, $this->logLevel);
+        }
+
+        return $balancer;
+    }
+
+    /**
+     * @param Monitor $monitor
+     * @param string  $metric
+     *
+     * @return $this
+     */
+    public function withMonitor(Monitor $monitor, $metric)
+    {
+        $this->monitor = $monitor;
+        $this->metric  = (string) new Identifier($metric);
+
+        return $this;
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     * @param string          $logLevel
+     *
+     * @return $this
+     */
+    private function withLogger(LoggerInterface $logger, $logLevel = LogLevel::INFO)
+    {
+        $this->logger   = $logger;
+        $this->logLevel = $logLevel;
+
+        return $this;
+    }
+
+    /**
+     * Add the features to the balancer
+     *
+     * @param BalancerInterface $balancer
+     * @param array             $config
+     */
+    private function addConfig(BalancerInterface $balancer, array $config)
+    {
+        foreach ($config as $feature => $percentages) {
+            $balancer->add($feature, $percentages);
+        }
+    }
+}
